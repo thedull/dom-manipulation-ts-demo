c6a6ec24dd0065a91a3c77b3c9f6bdfb70584a7c
Gabriel Trujillo <joaqt23@gmail.com>
2021-05-21 02:46:10 -0500

    Added address search

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import 'regenerator-runtime/runtime';
 import getZipInfo from './zipInfo';
 
+interface geoPoint {
+    display_name?: string;
+    lat?: string;
+    lon?: string;
+} 
+
 const app = document.getElementById('app');
 const header = document.createElement('h1');
 header.textContent = 'Hello, from TypeScript';
@@ -10,3 +16,37 @@ const code = '20500';
 const zipData = getZipInfo(code)
     .then(response => response.json())
     .then(data => console.log(data));
+
+const searchButton = document.getElementById('search');
+searchButton?.addEventListener('click', onSearch);
+
+function onSearch(): void {
+    const address = (document.getElementById('address') as HTMLInputElement).value;
+    if (address) {
+        console.log(address);
+        let geoData: string[];
+        fetch('https://nominatim.geocoding.ai/search.php?q='+ encodeURI(address))
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                geoData = data.map((item: geoPoint) => item.display_name);                
+                console.log(geoData);
+                clearGeoData();
+                renderGeoData(geoData);
+            });
+    }
+}
+
+function renderGeoData(geoData: string[]): void {
+    const list = document.createElement('ul');
+    geoData.forEach(entry => {
+        const item = document.createElement('li');
+        item.textContent = entry;
+        list.appendChild(item);    
+    });
+    document.getElementById('data')?.appendChild(list);
+}
+
+function clearGeoData(): void {
+    document.getElementById('data')?.firstChild?.remove();
+}
